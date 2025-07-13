69a350cffe2cae0516983b24bdd0d12c35ac4f6a
lee-leonardo <lee.leonardo.king@gmail.com>
2025-07-13 09:54:34 -0700

    feat: the sort and reduction of columns for the data csvs

--- data/mapping/steps.py
+++ data/mapping/steps.py
@@ -1,11 +1,58 @@
-def generateStep1(df):
-  print("hello")
+import pandas as pd
 
-  # s1 = df.copy(True)
-  # return s1
+def generateStep1(df):
+  s1 = df.copy(True)
+
+  # filter to be less than 1800
+  s1 = s1[s1['Year Published'] < 1800]
+
+  # filter out 0 (this is data that is incomplete for year)
+  s1 = s1[s1['Year Published'] != 0]
+
+  # bin to be grouped
+  s1 = s1.sort_values('Year Published')
+
+  # s1['bin'] = pd.cut(s1['Year Pubished'], bins=[])#, labels=[])
+
+  return s1[[
+    'Name',
+    'Year Published',
+    'Users Rated', # treat as modern players played
+    'Owned Users',
+    'Complexity Average',
+    'Mechanics'
+    ]
+  ]
 
 def generateStep2(df):
+  s2 = df.copy(True)
+  # modify here
+
+  # filter out 0 (this is data that is incomplete for year)
+  s2 = s2[s2['Year Published'] < 2000]
+  s2 = s2[s2['Year Published'] != 0]
+
+  # bin to be grouped
+  s2 = s2.sort_values('Year Published')
+
+  # s2['bin'] = pd.cut(s2['Year Pubished'], bins=[])#, labels=[])
+
+  return s2[[
+    'Name',
+    'Year Published',
+    'Users Rated', # treat as modern players played
+    'Owned Users',
+    'Complexity Average',
+    'Mechanics'
+    ]
+  ]
+
+def generateStep3(df):
   print("bye")
 
-  # s2 = df.copy(True)
-  # return s2
+  s3 = df.copy(True)
+
+  # modify here
+  s3 = s3[s3['Year Published'] < 2021]
+
+  return s3
