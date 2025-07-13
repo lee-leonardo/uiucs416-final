3440003e2a7466eb3ddace32173d9817bd0e954b
lee-leonardo <lee.leonardo.king@gmail.com>
2025-07-13 16:23:24 -0700

    adding bin information, bug in the cut code it seems for the binnin labels

--- data/mapping/steps.py
+++ data/mapping/steps.py
@@ -12,10 +12,12 @@ def generateStep1(df):
   # bin to be grouped
   s1 = s1.sort_values('Year Published')
 
-  # s1['bin'] = pd.cut(s1['Year Pubished'], bins=[])#, labels=[])
+  bins = pd.IntervalIndex.from_breaks([-3500, 0, 500, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800], closed="left")
+  s1['Bin'] = pd.cut(s1['Year Published'], bins=bins)
 
   return s1[[
     'Name',
+    'Bin',
     'Year Published',
     'Users Rated', # treat as modern players played
     'Owned Users',
@@ -36,6 +38,7 @@ def generateStep2(df):
   s2 = s2.sort_values('Year Published')
 
   # s2['bin'] = pd.cut(s2['Year Pubished'], bins=[])#, labels=[])
+  # pd.IntervalIndex.from_breaks([-3500, 0, 500, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800])
 
   return s2[[
     'Name',
@@ -54,5 +57,13 @@ def generateStep3(df):
 
   # modify here
   s3 = s3[s3['Year Published'] < 2021]
+  s3 = s3[s3['Year Published'] != 0] # still omit for this statistical graph
+
+  s3 = s3.sort_values('Year Published')
+
+  # pd.interval_range
+  # pd.union(pd.interval_range(1800, 1900, interval=10))
 
   return s3
+
+
