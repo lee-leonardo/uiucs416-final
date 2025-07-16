17a23ff3888654e9b11254c3b515a0bd3db6fcf0
lee-leonardo <lee.leonardo.king@gmail.com>
2025-07-16 00:09:53 -0700

    create helpers to get axes and prepare the document, finish first bar plot with animations

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @file This file is designed to initialize the view and
  */
 const HEIGHT = 400;
-const WIDTH = 400;
+const WIDTH = 600;
 const MARGIN = {
   top: 20,
   right: 20,
@@ -13,6 +13,10 @@ const MARGIN = {
 /**
  * Data Map
  */
+function get(stepId) {
+  return data[stepId]
+}
+
 function groupBarData(step) {
   const table = get(step)
   const groups = d3.group(table, d => d.Bin);
@@ -28,41 +32,117 @@ function generateCategoryKey() {
 }
 
 /**
+ * Syntax helpers
+ */
+function formatRange(range) {
+  return range.slice(1, -1).replace(', ', ' to ');
+}
+
+/**
+ * Node getters
+ */
+function getSvg() {
+  if (d3.select('#main').select('svg').nodes().length) {
+    return d3.select('#main').select('svg');
+  }
+
+  return d3.select("#main")
+    .append("svg")
+    .attr("id", "viewport")
+    .attr("height", HEIGHT)
+    .attr("width", WIDTH)
+    .attr("viewBox", [0, 0, WIDTH, HEIGHT]);
+}
+
+function getXAxis() {
+  if (getSvg().select('#x-axis').nodes().length) {
+    return getSvg().select('#x-axis');
+  }
+
+  return getSvg().append("g")
+    .attr('id', 'x-axis');
+}
+
+function getYAxis() {
+  if (getSvg().select('#y-axis').nodes().length) {
+    return getSvg().select('yx-axis');
+  }
+
+  return getSvg().append("g")
+    .attr('id', 'y-axis');
+}
+
+/**
  * Render
  * -------------------
  */
-
-let xAxis, yAxis;
-
 /**
+ * Rendering code references:
+ * https://d3-graph-gallery.com/graph/barplot_stacked_basicWide.html
+ *
  * Data from -3500BCE to 1800CE
  */
 function renderStep1() {
+  // Table data, but we will map to a simpler data structure for the bar plot
   const { groups } = groupBarData(0)
-  const labels = [];
 
+  /**
+   * @type {Array<{ bin: string, count: number }>}
+   */
+  const data = [];
+
+  let maxCount = -1;
   for (const key of groups.keys()) {
-    const range = [];
-    for (const val of key.matchAll(/(?<=[\[\(])-?\d+|(?<=,)-?\d+(?=[\]\)])/g)) {
-      range.push(val)
-    }
-    labels.push(range.join(" - "))
+    if (groups.get(key).length > maxCount) maxCount = groups.get(key).length;
 
-    // groups.get(key).length // group size
-
-    // TODO: find max
-    // TODO find a good scaling value for Y
-
-
-    let x = d3.scaleBand().domain(labels).range([0,100])
-    console.log(x)
-    // xAxis =
-
-    // let y = d3.
-    // yAxis =
+    data.push({
+      bin: key,
+      count: groups.get(key).length
+    });
   }
+  maxCount = Math.round(maxCount / 10) * 10
+
+  const x = d3.scaleBand().domain(groups.keys()).range([100, WIDTH-100])
+  const y = d3.scaleLinear().domain([0, maxCount]).range([HEIGHT - 100, 100])
+
+  // x axis
+  getXAxis()
+    .attr("transform", `translate(0, ${HEIGHT-100})`)
+    .call(d3.axisBottom(x).tickFormat(formatRange))//.tickSizeOuter(0))
+    .selectAll("text")
+      .attr("transform", "rotate(-45)")
+      .style("text-anchor", "end");
+
+  // y axis
+  getYAxis()
+    .attr("transform", `translate(100, 0)`)
+    .call(d3.axisLeft(y))
+
+  // labels
 
 
+  // create bar plots
+  getSvg()
+    .selectAll('.bar')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => x(d.bin))
+    .attr('width', x.bandwidth())
+    .attr('y', d => HEIGHT - 100)
+    .attr('height', _ => 0)
+    // TODO figure out if I like this
+    .attr("fill", "#4A90E2")
+    .attr("stroke", "#357ABD")
+    .attr("stroke-width", 1)
+  // animate bar plots
+    .transition()
+      .duration(500)
+      .ease(d3.easeElastic) // Bouncy easing
+      .attr("y", function (d) { return y(d.count); })
+      .attr("height", function (d) { return HEIGHT - 100 - y(d.count); })
+      .delay(function (d, i) { return (i * 50) })
 }
 
 /**
@@ -145,25 +225,16 @@ function reset() {
  * @function init
  * @description
  */
-function init() {
-  return d3.select("#main")
-    .append("svg")
-    .attr("id", "viewport")
-    .attr("height", HEIGHT)
-    .attr("width", WIDTH)
-    .attr("viewBox", "0 0 300 100")
-    ;
-}
-
 /**
  * Main
  */
 async function startScripts() {
   console.log('init');
   await initData();
-  const svg = init();
+  getSvg();
 
-  // TODO:
+  // Initialize the code to work off of the first step
+  renderStep1();
 }
 
 startScripts();
