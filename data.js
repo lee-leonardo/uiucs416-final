9b52f6afb3f69722d55202743886d34ee89f664b
lee-leonardo <lee.leonardo.king@gmail.com>
2025-07-31 01:49:14 -0700

    fix: forgot about data endpoint

--- data.js
+++ data.js
@@ -11,9 +11,9 @@ async function initData() {
   triggered = true;
 
   // Asynchronously pull the data for clonging
-  data.push(await d3.csv('/data/step_1.csv'))
-  data.push(await d3.csv('/data/step_2.csv'))
-  data.push(await d3.csv('/data/BGG_Data_Set_original.csv'));
+  data.push(await d3.csv('/uiucs416-final/data/step_1.csv'))
+  data.push(await d3.csv('/uiucs416-final/data/step_2.csv'))
+  data.push(await d3.csv('/uiucs416-final/data/BGG_Data_Set_original.csv'));
 
   window.DOMAIN_ORDINAL = Array.from(d3.union(data[2].flatMap(e => e['Domains'].split(', '))))
   window.DOMAIN_ORDINAL.sort()
